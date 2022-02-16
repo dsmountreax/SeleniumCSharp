a8b00b6cba678a2941e575373b81537ced707157
Enrique Oreiro <dsmountreax@gmail.com>
2022-02-16 10:48:12 -0500

    Class About Arrays and For Loops

---
+++ CSharpFundamentals/Program5.cs
@@ -0,0 +1,20 @@
+﻿using System;
+// Array
+String[] a = { "Hello", "Bye", "Rahul", "Shetty" };
+int[] b = { 1,2,3,4,5};
+String[] a1 = new String[4];
+a1[0] = "hello";
+a1[1] = "bye";
+
+Console.WriteLine(a[1]);
+
+for(int i=0;i<a.Length; i++)
+{
+    Console.WriteLine(a[i]);
+    if(a[i]=="Rahul")
+    {
+        Console.WriteLine("Match Found");
+        break;
+    } 
+}
+
