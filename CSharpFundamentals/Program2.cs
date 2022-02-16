0ff9acf0e3e8c9c72a28022a41cb8363410507e5
Enrique Oreiro <dsmountreax@gmail.com>
2022-02-15 19:12:17 -0500

    Class About Selective File

--- CSharpFundamentals/Program2.cs
+++ CSharpFundamentals/Program2.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿using CSharpFundamentals;
+using System;
 Console.WriteLine("I am in the second program");
 // Global scope of main
 // Seleccionando el archivo con click derecho e indicando Build action se cambia a None
+// Nueva clase para main selectivo
+Program p = new Program();
+p.getData();
+// todas las clases asociadas deben estar en compilar sino va a generar error
+
