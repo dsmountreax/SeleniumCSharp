ba35d1d5ef44883ac8b23a8af97621fa25c4534f
Enrique Oreiro <dsmountreax@gmail.com>
2022-02-16 11:08:24 -0500

    Arraylist and Enhaced For Loop

---
+++ CSharpFundamentals/Program6.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+
+ArrayList a = new ArrayList();
+a.Add("Hello");
+a.Add("Bye");
+a.Add("Rahul");
+a.Add("Apple");
+
+foreach(String item in a)
+{
+    Console.WriteLine(item);
+}
+
+Console.WriteLine(a.Contains("Rahul"));
+a.Sort();
+Console.WriteLine("After Sorting");
+
+foreach (String item in a)
+{
+    Console.WriteLine(item);
+}
