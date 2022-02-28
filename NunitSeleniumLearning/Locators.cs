800cb9a5549c921bfb86e5ca2fb9a58209e7bcf9
Enrique Oreiro <dsmountreax@gmail.com>
2022-02-28 09:33:05 -0500

    Locators

---
+++ NunitSeleniumLearning/Locators.cs
@@ -0,0 +1,39 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebDriverManager.DriverConfigs.Impl;
+
+namespace NunitSeleniumLearning
+{
+    class Locators
+    {
+        IWebDriver driver;
+
+        [SetUp]
+
+        public void StartBrowser()
+        {
+            new WebDriverManager.DriverManager().SetUpDriver(new ChromeConfig());
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Url = "https://rahulshettyacademy.com/loginpagePractise/";
+        }
+
+        [Test]
+
+    public void LocatorsIdentification()
+        {
+            driver.FindElement(By.CssSelector("#username")).SendKeys("usuario");
+            driver.FindElement(By.CssSelector("#password")).SendKeys("Contraseña");
+            driver.FindElement(By.CssSelector("#signInBtn")).Click();
+            driver.Close();
+        }
+
+
+    }
+}
