99fdbe64ea9ed3360a138c4209f6248cce30da3a
Enrique Oreiro <dsmountreax@gmail.com>
2022-03-01 09:03:43 -0500

    Static Dropdowns

---
+++ NunitSeleniumLearning/FunctionalTest.cs
@@ -0,0 +1,41 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebDriverManager.DriverConfigs.Impl;
+
+namespace NunitSeleniumLearning
+{
+    class FunctionalTest
+    {
+        IWebDriver driver;
+        [SetUp]
+
+        public void StartBrowser()
+        {
+            new WebDriverManager.DriverManager().SetUpDriver(new ChromeConfig());
+            driver = new ChromeDriver();
+            //driver.Manage().Timeouts().ImplicitWait=TimeSpan.FromSeconds(3);
+            driver.Manage().Window.Maximize();
+            driver.Url = "https://rahulshettyacademy.com/loginpagePractise/";
+        }
+
+        [Test]
+        public void dropdown()
+        {
+            IWebElement dropdown =driver.FindElement(By.CssSelector("[data-style]"));
+            SelectElement s = new SelectElement(dropdown);
+            s.SelectByText("Teacher");
+            s.SelectByValue("stud"); // zero index is equal at the first option
+            s.SelectByIndex(2);
+            //driver.Close();
+
+        }
+
+    }
+}
