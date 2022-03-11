68262c1d2cfdac0f62f92499980baeef078690d9
Enrique Oreiro <dsmountreax@gmail.com>
2022-03-10 21:14:23 -0500

    Child Window

---
+++ NunitSeleniumLearning/WindowHandlers.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebDriverManager.DriverConfigs.Impl;
+
+namespace NunitSeleniumLearning
+{
+    class WindowHandlers
+    {
+        IWebDriver driver;
+        [SetUp]
+        public void StartBrowser()
+        {
+            new WebDriverManager.DriverManager().SetUpDriver(new ChromeConfig());
+            driver = new ChromeDriver();
+
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+            driver.Url = "https://rahulshettyacademy.com/loginpagePractise/";
+        }
+
+        [Test]
+        public void WindowHandle()
+        {
+            driver.FindElement(By.CssSelector(".blinkingText")).Click();
+            Assert.AreEqual(2, driver.WindowHandles.Count);
+            String childWindowName = driver.WindowHandles[1];
+            driver.SwitchTo().Window(childWindowName);
+            TestContext.Progress.WriteLine(driver.FindElement(By.CssSelector(".red")).Text);
+
+        }
+    }
+}
