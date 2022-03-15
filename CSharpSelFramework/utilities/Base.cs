8d0d3726b15bff0d02e2a3a55b7fd5ab2b086c64
Enrique Oreiro <dsmountreax@gmail.com>
2022-03-15 13:35:29 -0500

    Init browser

--- CSharpSelFramework/utilities/Base.cs
+++ CSharpSelFramework/utilities/Base.cs
@@ -7,6 +7,8 @@ using System.Threading.Tasks;
 using OpenQA.Selenium;
 using WebDriverManager.DriverConfigs.Impl;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Edge;
 
 namespace CSharpSelFramework.utilities
 {
@@ -16,11 +18,37 @@ namespace CSharpSelFramework.utilities
         [SetUp]
         public void StartBrowser(){
 
-            new WebDriverManager.DriverManager().SetUpDriver(new ChromeConfig());
-            driver = new ChromeDriver();
+            InitBrowser("Chrome");
+
             driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
             driver.Manage().Window.Maximize();
             driver.Url = "https://rahulshettyacademy.com/loginpagePractise/";
+        }
+
+        public void InitBrowser(string browserName)
+        {
+            switch(browserName)
+            {
+                case "Firefox":
+                    new WebDriverManager.DriverManager().SetUpDriver(new FirefoxConfig());
+                    driver = new FirefoxDriver();
+                    break;
+
+                case "Chrome":
+                    new WebDriverManager.DriverManager().SetUpDriver(new ChromeConfig());
+                    driver = new ChromeDriver();
+                    break;
+
+                case "Edge":
+                    driver = new EdgeDriver();
+                    break;
+            }
+        }
+
+        [TearDown]
+        public void AfterTest()
+        {
+            driver.Quit();
         }
     }
 }
