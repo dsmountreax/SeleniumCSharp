96df5f88749a28f27fbacfd951cdd325411193e9
Enrique Oreiro <dsmountreax@gmail.com>
2022-03-16 00:11:25 -0500

    Json Data and Parsers

---
+++ CSharpFundamentals/JsonReader.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+String myJsonString =File.ReadAllText("testData.json");
+
+            var jsonObject=JToken.Parse(myJsonString);
+            Console.WriteLine(jsonObject.SelectToken("username").Value<string>());
